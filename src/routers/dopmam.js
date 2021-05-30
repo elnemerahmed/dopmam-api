ebec196ef9122868ac671f2d1c031f0dcfb68952
Ahmed ElNemer <39246763+elnemerahmed@users.noreply.github.com>
2021-05-30 11:21:55 +0300

    Update dopmam.js

--- src/routers/dopmam.js
+++ src/routers/dopmam.js
@@ -4,4 +4,41 @@ const authentication = require( '../middleware/authentication' );
 
 const router = new express.Router();
 
+router.post( '/dopmam/report/sign', authentication, async ( req, res ) => {
+    try {
+        const { user } = req;
+        const { name, organization } = user;
+        const { reportId } = req.body;
+ 
+        if(!authorizedOR(user, ["dopmam_medical_lead", "dopmam_medical", "dopmam_financial_lead", "dopmam_financial"])) {
+            throw new Error();
+        }
+
+        const result = await signReport(name, organization, reportId);
+
+        res.status( 200 ).send(result);
+    } catch ( error ) {
+        res.status( 500 ).send(error);
+    }
+} );
+
+router.get( '/dopmam/reports', authentication, async ( req, res ) => {
+    try {
+        const { user } = req;
+        const { name, organization } = user;
+        const { reportId } = req.body;
+ 
+        if(!authorizedOR(user, ["dopmam_medical_lead", "dopmam_medical", "dopmam_financial_lead", "dopmam_financial"])) {
+            throw new Error();
+        }
+
+        const result = await getRports(name, organization);
+        res.status( 200 ).send(result);
+    } catch ( error ) {
+        res.status( 500 ).send(error);
+    }
+} );
+
 module.exports = router;
+
+
