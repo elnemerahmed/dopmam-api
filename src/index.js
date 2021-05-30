7192bc345007ad36b5740cb8efe377eb8acfd1ee
Ahmed ElNemer <39246763+elnemerahmed@users.noreply.github.com>
2021-05-30 11:20:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require( 'express' );
 
 const userRouter = require( './routers/user' );
 const hospitalRouter = require( './routers/hospital' );
-//const dopmamRouter = require( './routers/dopmam' );
+const dopmamRouter = require( './routers/dopmam' );
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 app.use( express.json() );
 app.use( userRouter );
 app.use( hospitalRouter );
-//app.use( dopmamRouter );
+app.use( dopmamRouter );
 
 app.listen( port, () => {
     console.log( `Server started at: http://localhost:${ port }` );
